2346216d003a2a09d4613020a80fd608dab4a8e5
andres saldana <andres.saldana.aguilar@gmail.com>
2020-05-03 10:23:46 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7e564d3e47cc0ab88d26a9ab9d62f267.js"
+  "/andresSaldanaAguilar/andresSaldanaAguilar.github.io/precache-manifest.c3e5783f66b6e9ef144d090fd629e9eb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/andresSaldanaAguilar/andresSaldanaAguilar.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
